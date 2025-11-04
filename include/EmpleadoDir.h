bfbd63d4bd1f2b56f3db739e3393bc91b4c63a1c
Aarxn10 <penaaaron792@gmail.com>
2025-11-04 12:51:34 -0600

    Nueva clase
    Empleado dir

---
+++ include/EmpleadoDir.h
@@ -0,0 +1,19 @@
+#ifndef EMPLEADODIR_H
+#define EMPLEADODIR_H
+#include <string>
+#include "Empleado.h"
+using namespace std
+
+class EmpleadoDir : publicEmpleado
+{
+    public:
+        //Constructor
+        EmpleadoDir();
+        virtual ~EmpleadoDir();
+
+    protected:
+
+    private:
+};
+
+#endif // EMPLEADODIR_H
---
+++ src/EmpleadoDir.cpp
@@ -0,0 +1,11 @@
+#include "EmpleadoDir.h"
+
+EmpleadoDir::EmpleadoDir()
+{
+    //ctor
+}
+
+EmpleadoDir::~EmpleadoDir()
+{
+    //dtor
+}
