f9859140c0c40a69f0abdde801b5ec720664fe76
Aarxn10 <penaaaron792@gmail.com>
2025-11-04 12:58:54 -0600

    Clase
    Nueva clase .h y ,cpp

--- include/EmpleadoDir.h
+++ include/EmpleadoDir.h
@@ -1,19 +1,16 @@
-#ifndef EMPLEADODIR_H
-#define EMPLEADODIR_H
+#ifndef EMPLEDIR_H
+#define EMPLEDIR_H
 #include <string>
 #include "Empleado.h"
-using namespace std
-
-class EmpleadoDir : publicEmpleado
-{
-    public:
-        //Constructor
-        EmpleadoDir();
-        virtual ~EmpleadoDir();
-
-    protected:
-
-    private:
+using namespace std;
+class EmpleDir : public Empleado {
+private:
+   float bono;  // Atributo propio del empleado directivo
+public:
+   // Constructor
+   EmpleDir(string n = "", float b = 0.0);
+   // Métodos set y get
+   void setBono(float b);
+   float getBono() const;
 };
-
-#endif // EMPLEADODIR_H
+#endif
--- src/EmpleadoDir.cpp
+++ src/EmpleadoDir.cpp
@@ -1,11 +1,16 @@
-#include "EmpleadoDir.h"
-
-EmpleadoDir::EmpleadoDir()
-{
-    //ctor
-}
-
-EmpleadoDir::~EmpleadoDir()
-{
-    //dtor
-}
+#ifndef EMPLEDIR_H
+#define EMPLEDIR_H
+#include <string>
+#include "Empleado.h"
+using namespace std;
+class EmpleDir : public Empleado {
+private:
+   float bono;  // Atributo propio del empleado directivo
+public:
+   // Constructor
+   EmpleDir(string n = "", float b = 0.0);
+   // Métodos set y get
+   void setBono(float b);
+   float getBono() const;
+};
+#endif
